3eef94544806d34c46484d2aadb82b19cb07e8b9
koroshiG <michal.piwowarski10@gmail.com>
2019-04-04 14:36:37 +0200

    sterowanie

--- script.js
+++ script.js
@@ -41,45 +41,6 @@ function trackRender(ctx, pat) {
 
 }
 
-function race(ctx, startPos, key, color) {
-    xSpeed = 0
-    ySpeed = 0
-    xPos = startPos[0]
-    yPos = startPos[1]
-
-    document.addEventListener("keydown", (e) => {
-        if (e.key == key) {
-            if(xSpeed > Math.PI/2){
-            xSpeed = -Math.PI/2
-            }
-            xSpeed += (Math.PI/33)
-        }
-    })
-
-    frame = () => {      
-
-        x = Math.cos(xSpeed)
-        y = -(1 - x)
-        console.log("y: " + y, "x: " + x)
-        
-        ctx.strokeStyle = color
-        xPos += x
-        yPos += y
-        ctx.lineTo(xPos, yPos)
-        ctx.stroke()
-
-        requestAnimationFrame(frame)
-    }
-
-    frame()
-}
-
-function start(){
-    playerNumber = 0
-    p1 = new Player(1, "green", "q")
-    race(ctx, p1.startPos, p1.klawsz, p1.kolor)
-}
-
 class Player {
     constructor(numer, kolor, klawisz) {
         this.numer = numer
@@ -90,6 +51,53 @@ class Player {
 
     init() {
         this.startPos = [stoProcentW * 0.5, stoProcentH * 0.78 + (playerNumber * stoProcentH * 0.03)]
+        playerNumber++
+
+        var canvas = document.createElement("canvas")
+        canvas.height = stoProcentH
+        canvas.width = stoProcentW
+        canvas.style.position = "absolute"
+        canvas.style.top = "0px"
+        canvas.style.left = "0px"
+        canvas.id = "p" + this.numer
+        document.body.appendChild(canvas)
+        ctx = document.getElementById("p" + this.numer)
+        this.ctx = ctx.getContext("2d")
+
+        this.race()
+    }
+
+    race() {
+        this.angle = 0
+        this.speed = .5
+        this.xPos = this.startPos[0]
+        this.yPos = this.startPos[1]
+        this.x = 0
+        this.y = 0
+
+        document.addEventListener("keypress", (e) => {
+            if (e.key == this.klawsz) {
+                this.angle += (Math.PI / 18)
+            }
+        })
+
+        this.frame = () => {
+
+            this.x = Math.cos(this.angle) * this.speed
+            this.y = Math.sin(-this.angle) * this.speed
+            //console.log("y: " + this.y, "x: " + this.x)
+
+            this.ctx.lineWidth = stoProcentH / 100
+            this.ctx.strokeStyle = this.kolor
+            this.xPos += this.x
+            this.yPos += this.y
+            this.ctx.lineTo(this.xPos, this.yPos)
+            this.ctx.stroke()
+
+            requestAnimationFrame(this.frame)
+        }
+
+        this.frame()
     }
 }
 
@@ -97,37 +105,39 @@ document.onreadystatechange = () => {
     if (document.readyState == "complete") {
         stoProcentH = window.innerHeight
         stoProcentW = window.innerWidth
-        document.body.innerHTML = '';
-        var canvas = document.createElement("canvas")
-        canvas.height = stoProcentH
-        canvas.width = stoProcentW
-        canvas.id = "canvas"
-        document.body.appendChild(canvas)
-        ctx = document.getElementById("canvas")
-        ctx = ctx.getContext("2d")
-        img = new Image()
-        img.src = "img/grass.jpg"
-        img.addEventListener("load", () => {
-            trackRender(ctx, img)
-        })
+        playerNumber = 0
+        document.getElementById("addPlyr").onclick = addPlayer
+        document.getElementById("start").onclick = mapStart
     }
 }
 
-window.onresize = () => {
-    stoProcentH = window.innerHeight
-    stoProcentW = window.innerWidth
+function addPlayer(){
+    kolor = document.createElement("input")
+    kolor.id = "kolor"
+
+    playerNumber++
+
+}
+
+function mapStart() {
     document.body.innerHTML = '';
-    var canvas = document.createElement("canvas")
+    canvas = document.createElement("canvas")
     canvas.height = stoProcentH
     canvas.width = stoProcentW
     canvas.id = "canvas"
     document.body.appendChild(canvas)
-    ctx = document.getElementById("canvas")
-    ctx = ctx.getContext("2d")
-    trackRender(ctx)
+    canvas = document.getElementById("canvas")
+    ctx = canvas.getContext("2d")
     img = new Image()
     img.src = "img/grass.jpg"
     img.addEventListener("load", () => {
         trackRender(ctx, img)
     })
+}
+
+window.onresize = () => {
+    stoProcentH = window.innerHeight
+    stoProcentW = window.innerWidth
+    canvas = document.getElementById("canvas")
+    canvas.innerHTML = '';
 }
